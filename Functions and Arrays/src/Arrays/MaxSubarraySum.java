614a2dc3b0e5146425a2de381cc877aa869a86d3
ramanrdx14 <ramanrs1410@gmail.com>
2026-08-03 00:09:31 +0530

    Functions and Arrays

--- Functions and Arrays/src/Arrays/MaxSubarraySum.java
+++ Functions and Arrays/src/Arrays/MaxSubarraySum.java
@@ -14,11 +14,24 @@ public class MaxSubarraySum {
     }
     public static void maxSubarraySum2(int[] arr){
         //Kadane
+
+        int maxSubarraySum = Integer.MIN_VALUE;
+        int currentSum     = 0;
+        for(int i:arr){
+
+            currentSum = currentSum + i;
+            maxSubarraySum = Math.max(maxSubarraySum,currentSum);
+
+            if(currentSum < 0){
+                currentSum = 0; //new subarray sum creation from next index
+            }
+        }
+        System.out.println(maxSubarraySum);
     }
 
     public static void main(String[] args) {
         int[] arr = {-1,5,-8,11};
         maxSubarraySum1(arr);
-        //maxSubarraySum2(arr);
+        maxSubarraySum2(arr);
     }
 }
